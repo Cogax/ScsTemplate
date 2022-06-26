b51eb6d77448543efee170189f7d218ff2459413
Andreas Gyr <agyr@techgroup.ch>
2022-06-26 21:33:11 +0200

    cleanup

--- Cogax.SelfContainedSystem.Template.Core.Application/Common/Consistency/UnitOfWork.cs
+++ Cogax.SelfContainedSystem.Template.Core.Application/Common/Consistency/UnitOfWork.cs
@@ -1,5 +1,3 @@
-using Cogax.SelfContainedSystem.Template.Core.Application.Common.Ports;
-
 namespace Cogax.SelfContainedSystem.Template.Core.Application.Common.Consistency;
 
 public interface IUnitOfWorkFactory
